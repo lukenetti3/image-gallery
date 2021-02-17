5d2172453078f2a21aacef8ded98f67da5457bea
Luke Netti <lukenetti@hey.com>
2021-02-17 18:53:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { createApi } from "unsplash-js"
+import { useState } from "react"
 
 function App() {
+  const [photos, setPhotos] = useState(null)
+
+  const api = createApi({
+    accessKey: "c998EbR5HC_BTOhQ8PPRUBF30o30HM50e-Zw0HPt4Z4",
+  })
+
+  api.search
+    .getPhotos({ query: "dog" })
+    .then((result) => console.log(result.response))
+
+  // api.photos.getRandom().then((result) => console.log(result.response))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Image Gallery App</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
