f2df72d441d00655a1c4a277957e366e37df41b2
Luke Netti <lukenetti@hey.com>
2021-02-18 19:13:22 -0500

    grid of unsplash images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createApi } from "unsplash-js"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [photos, setPhotos] = useState(null)
+  const [photos, setPhotos] = useState([])
+  const [isModal, setIsModal] = useState(false)
+  const [modalImage, setModalImage] = useState("")
+  const [pageNum, setPageNum] = useState(0)
 
   const api = createApi({
     accessKey: "c998EbR5HC_BTOhQ8PPRUBF30o30HM50e-Zw0HPt4Z4",
   })
 
-  api.search
-    .getPhotos({ query: "dog" })
-    .then((result) => console.log(result.response))
+  useEffect(() => {
+    api.search
+      .getPhotos({ query: "cats" })
+      .then((result) => setPhotos(result.response.results))
+      .catch(() => {
+        console.log("something is broke")
+      })
+  }, [pageNum])
 
-  // api.photos.getRandom().then((result) => console.log(result.response))
+  const openLightbox = (photo) => {
+    setModalImage(photo)
+
+    setIsModal(true)
+  }
 
   return (
     <div className='App'>
-      <h1>Image Gallery App</h1>
+      <div className='container'>
+        <h1 style={{ textAlign: "center" }}>Image Gallery App</h1>
+        <div className='image-container'>
+          {console.log(photos)}
+          {photos.map((photo) => {
+            return (
+              <img
+                src={photo.urls.full}
+                alt={photo.alt_description}
+                key={photo.id}
+                onClick={() => openLightbox(photo)}
+              />
+            )
+          })}
+        </div>
+      </div>
+      {isModal ? (
+        <div className='lightbox'>
+          <h1>{modalImage.alt_description}</h1>
+          <img
+            src={modalImage.urls.small}
+            alt={modalImage.alt_description}
+            className='lightbox-image'
+          />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   )
 }
 
 export default App
+
+// TODO:
+// - Add lightbox to images when clicked
+// - Setup infinite scroll
+// - Change mode between list and grid
